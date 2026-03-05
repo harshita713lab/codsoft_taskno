4319953baf7b9cedd41c40b34b670dd1e3de0fde
harshita rathore <harshitar713@gmail.com>
2026-03-06 00:09:08 +0530

    Final Fix: Restored all missing files and secured key

--- Task 1 Level 2/task/backend/models/Job.js
+++ Task 1 Level 2/task/backend/models/Job.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 
-// Yeh humara Blueprint hai (Job kaisi dikhegi)
 const jobSchema = new mongoose.Schema({
   title: { type: String, required: true },
   company: { type: String, required: true },
@@ -8,5 +7,4 @@ const jobSchema = new mongoose.Schema({
   salary: { type: String, required: true }
 });
 
-// Is blueprint ko export kar rahe hain taaki humari server file iska use kar sake
 module.exports = mongoose.model('Job', jobSchema);
